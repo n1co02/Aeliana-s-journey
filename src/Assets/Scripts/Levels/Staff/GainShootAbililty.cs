b7dc8dc04b567b5fa72e9b32fc183fa467f1fa26
n1co02 <nicolasostermann@web.de>
2024-01-25 22:44:44 +0100

    delete src

--- src/Assets/Scripts/Levels/Staff/GainShootAbililty.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class GainShootAbililty : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        PlayerPrefs.SetInt("shootAbility", 1);
-    }
-}
